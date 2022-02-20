b5f73930136d3be99eb5310f45b09f83099c55f2
UTABPE <47221260+UTABPE@users.noreply.github.com>
2022-02-20 12:01:15 +0600

    Update PipeMove.cs

--- Assets/Scripts/FlappyBird/PipeMove.cs
+++ Assets/Scripts/FlappyBird/PipeMove.cs
@@ -10,7 +10,4 @@ public class PipeMove : MonoBehaviour
     {
         transform.Translate(-speed * Time.deltaTime, 0, 0); // движение труб
     }
-
-    // Подписывайся на канал ICE CREAM
-    // Нашел неточность - напиши мне на почту или в комменты! Спасибки!
 }
